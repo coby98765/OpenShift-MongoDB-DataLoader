e81268fb4f8be4f80ff26ab97f2b40cf49d4d5c2
Yossi Ben Haim <v0585204770@gmail.com>
2025-08-20 11:54:49 +0300

    create the fixed DAL

--- services/data_loader/DAL.py
+++ services/data_loader/DAL.py
@@ -1,72 +1,88 @@
+from bson import ObjectId
 from pymongo import MongoClient
-import datetime
-
+from models.soldier_model import Soldier
 
 class DAL:
 
     def __init__(self):
         self.client = None
 
+
     def connect(self):
         self.client = MongoClient("mongodb://localhost:27017")
+
 
     def close_conn(self):
         self.client.close()
 
+
     def get_all_data(self):
-        self.connect()
-        db = self.client["enemy_soldiers"]
-        collection = db["soldier_details"]
-        result = list(collection.find({}))
-        self.close_conn()
-        return str(result)
-
-    def get_data_by_id(self,id):
-        self.connect()
-        db = self.client["enemy_soldiers"]
-        collection = db["soldier_details"]
-        result = list(collection.find({'id':id}))
-        self.close_conn()
-        return str(result)
-
-    def insert_data(self):
-        self.connect()
-        post = self.get_data()
-        db = self.client["enemy_soldiers"]
-        posts = db["soldier_details"]
-        post_id = posts.insert_one(post)
-        self.close_conn()
-        return 'the data is inserted!'
+        try:
+            self.connect()
+            db = self.client["enemy_soldiers"]
+            collection = db["soldier_details"]
+            result = list(collection.find({}))
+            return {"result": result}
+        except Exception as e:
+            print(e)
+            raise {"Error": e}
+        finally:
+            self.close_conn()
 
 
-    def update_data(self, id, first_name, last_name, phone_number, rank):
-        self.connect()
-        db = self.client["enemy_soldiers"]
-        posts = db["soldier_details"]
-        post_id = posts.updateOne({'_id': id}, { '$set': {'first_name': first_name, 'kast_name':last_name, 'phone_number':phone_number, 'rank':rank}} )
-        self.close_conn()
-        return "the database is updated!"
-
-    def delete_data_by_id(self,id):
-        self.connect()
-        db = self.client["enemy_soldiers"]
-        db.soldier_details.deleteOne({'_id':id})
-        self.close_conn()
-        return 'the data is deleted!'
-
-    def get_data(self):
-        return {
-            "_id":203,
-            "first_name": "yossi",
-            "last_name": "SHBB adb  ad!",
-            "phone_number": '0585204770',
-            "rank": "iushsz",
-        }
+    def get_data_by_id(self,soldier_id):
+        try:
+            self.connect()
+            db = self.client["enemy_soldiers"]
+            collection = db["soldier_details"]
+            result = list(collection.find({'_id':ObjectId(soldier_id)}))
+            return {'result':result}
+        except Exception as e:
+            print(e)
+            raise {"Error": e}
+        finally:
+            self.close_conn()
 
 
+    def insert_data(self, soldier : Soldier):
+        try:
+            self.connect()
+            db = self.client["enemy_soldiers"]
+            collection = db["soldier_details"]
+            insert_data = soldier.get_query()
+            inserted_id = collection.insert_one(insert_data["query"]).inserted_id
+            return {"added":inserted_id}
+        except Exception as e:
+            print(e)
+            raise {"Error": e}
+        finally:
+            self.close_conn()
 
 
-a = DAL()
-a.delete_data_by_id(203)
-print(a.get_all_data())
+    def update_data(self, soldier : Soldier):
+        try:
+            self.connect()
+            db = self.client["enemy_soldiers"]
+            collection = db["soldier_details"]
+            update_data = soldier.get_query()
+            posted = collection.update_one({'_id': ObjectId(update_data['id'])}, { '$set': update_data['query']} )
+            return {"updated":posted}
+        except Exception as e:
+            print(e)
+            raise {"Error": e}
+        finally:
+            self.close_conn()
 
+
+    def delete_data_by_id(self, soldier_id):
+        try:
+            self.connect()
+            db = self.client["enemy_soldiers"]
+            collection = db["soldier_details"]
+            result = collection.delete_one( {'_id' : ObjectId(soldier_id)}).deleted_count
+            return {"deleted":result}
+        except Exception as e:
+            print(e)
+            raise {"Error": e}
+        finally:
+            self.close_conn()
