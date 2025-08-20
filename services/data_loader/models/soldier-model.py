387e2f3508e006926ec6c26d820f6009b2905bb8
Gabi Thaler <gmth054@gmail.com>
2025-08-20 11:55:17 +0300

    complited the maneger

--- services/data_loader/models/soldier-model.py
+++
@@ -1,7 +0,0 @@
-class Soldier:
-    def __init__(self,id,first_name,last_name,phone_number,rank):
-        self.id=id
-        self.first_name=first_name
-        self.last_name=last_name
-        self.phone_number=phone_number
-        self.rank=rank
